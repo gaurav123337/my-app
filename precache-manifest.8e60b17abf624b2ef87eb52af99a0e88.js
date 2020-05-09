30a2515f816afe26827a5c9e08afde5fb7e13537
Gaurav <gaurav.123337@gmail.com>
2020-05-10 00:46:34 +0530

    Updates

---
+++ precache-manifest.8e60b17abf624b2ef87eb52af99a0e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15872a9d1221f6de03b3b913317a9235",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a0c272afaf46d709ef63",
+    "url": "/my-app/static/css/2.81e2c8a5.chunk.css"
+  },
+  {
+    "revision": "d67c3e64633ca55d7536",
+    "url": "/my-app/static/css/main.4061f532.chunk.css"
+  },
+  {
+    "revision": "a0c272afaf46d709ef63",
+    "url": "/my-app/static/js/2.9005491a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.9005491a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d67c3e64633ca55d7536",
+    "url": "/my-app/static/js/main.3962109b.chunk.js"
+  },
+  {
+    "revision": "d21a59192a6355194f80",
+    "url": "/my-app/static/js/runtime-main.f5212b54.js"
+  }
+]);
