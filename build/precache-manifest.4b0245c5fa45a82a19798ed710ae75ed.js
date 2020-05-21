ceaa7921c18c2d1bf4b5f4fb08633a42b4561ab4
Gaurav <gaurav.123337@gmail.com>
2020-05-21 11:58:54 +0530

    build

---
+++ build/precache-manifest.4b0245c5fa45a82a19798ed710ae75ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3c380963cf88f4b608a2416c49d634",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d760a0a7c80e8e40bb74",
+    "url": "/my-app/static/css/2.81e2c8a5.chunk.css"
+  },
+  {
+    "revision": "e0acf8bd807e2b28fe18",
+    "url": "/my-app/static/css/main.4061f532.chunk.css"
+  },
+  {
+    "revision": "d760a0a7c80e8e40bb74",
+    "url": "/my-app/static/js/2.7447e0a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.7447e0a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0acf8bd807e2b28fe18",
+    "url": "/my-app/static/js/main.26d4b47e.chunk.js"
+  },
+  {
+    "revision": "d21a59192a6355194f80",
+    "url": "/my-app/static/js/runtime-main.f5212b54.js"
+  }
+]);
