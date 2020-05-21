50d027254d7215a1f4e4697bbb7cc8f017445ccd
Gaurav <gaurav.123337@gmail.com>
2020-05-21 14:09:14 +0530

    new build

--- build/precache-manifest.f572e15a80866d1b5a322a9cf7e067a8.js
+++ build/precache-manifest.f572e15a80866d1b5a322a9cf7e067a8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a2f152844cbff6fbe44c71437598105",
+    "revision": "2f0f0f176b5147637f458d168ac6a70c",
     "url": "/my-app/index.html"
   },
   {
-    "revision": "d760a0a7c80e8e40bb74",
+    "revision": "41b8efb14688ae90593a",
     "url": "/my-app/static/css/2.81e2c8a5.chunk.css"
   },
   {
-    "revision": "7a0d3950dd124c34ab2f",
+    "revision": "650b522834ad3f602ef7",
     "url": "/my-app/static/css/main.4061f532.chunk.css"
   },
   {
-    "revision": "d760a0a7c80e8e40bb74",
-    "url": "/my-app/static/js/2.7447e0a8.chunk.js"
+    "revision": "41b8efb14688ae90593a",
+    "url": "/my-app/static/js/2.c7a2ad03.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/my-app/static/js/2.7447e0a8.chunk.js.LICENSE.txt"
+    "url": "/my-app/static/js/2.c7a2ad03.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7a0d3950dd124c34ab2f",
-    "url": "/my-app/static/js/main.45e0b016.chunk.js"
+    "revision": "650b522834ad3f602ef7",
+    "url": "/my-app/static/js/main.59e99de4.chunk.js"
   },
   {
     "revision": "d21a59192a6355194f80",
